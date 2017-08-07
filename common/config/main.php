551f823ef0f031d0ffe05032db36959e9ae639b1
Sofiane Medjkoune <sofiane.medjkoune@protonmail.com>
2017-08-07 15:53:53 +0200

    Added a user login before any action

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,20 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'as beforeRequest' => [  //if guest user access site so, redirect to login page.
+        'class' => 'yii\filters\AccessControl',
+        'rules' => [
+            [
+                'actions' => ['login', 'error'],
+                'allow' => true,
+            ],
+            [
+                'allow' => true,
+                'roles' => ['@'],
+            ],
+        ],
+    ],
+    
 ];
+
+
